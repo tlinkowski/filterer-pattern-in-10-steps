4f1a4adbf80dd8b6b4ee495e4b64d8c94dd84ce3
Tomasz Linkowski <t.linkowski@gmail.com>
2019-01-31 21:28:31 +0100

    Step 10: Detection Time

--- ProbabilisticIssueWiseText.java
+++ ProbabilisticIssueWiseText.java
@@ -1,8 +1,11 @@
+import java.time.Duration;
 import java.util.List;
 
 interface ProbabilisticIssueWiseText extends IssueWiseText {
   @Override
   List<? extends ProbableIssue> issues();
+
+  Duration detectionTime();
 
   @Override
   Filterer<? extends ProbabilisticIssueWiseText, ? extends ProbableIssue> filtered();
