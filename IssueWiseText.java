31e9139a1f29df0c2a51328aa05a62046d36fbf0
Tomasz Linkowski <t.linkowski@gmail.com>
2019-01-31 21:26:04 +0100

    Step 7: Filtering by Issue Type

--- IssueWiseText.java
+++ IssueWiseText.java
@@ -1,7 +1,10 @@
 import java.util.List;
+import java.util.function.Predicate;
 
 interface IssueWiseText {
   String text(); // e.g. "You migth know it."
 
   List<? extends Issue> issues(); // e.g. ["migth"]
+
+  IssueWiseText filtered(Predicate<? super Issue> issueFilter);
 }
--- ProbabilisticIssueWiseText.java
+++ ProbabilisticIssueWiseText.java
@@ -1,6 +1,10 @@
 import java.util.List;
+import java.util.function.Predicate;
 
 interface ProbabilisticIssueWiseText extends IssueWiseText {
   @Override
   List<? extends ProbableIssue> issues();
+
+  @Override
+  ProbabilisticIssueWiseText filtered(Predicate<? super Issue> issueFilter);
 }
